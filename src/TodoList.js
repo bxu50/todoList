e10131046cdf819b7b50b298ddbed508a0dfa014
Benshuai Xu <bxu50@student.monash.edu>
2019-09-11 22:47:10 +1000

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  render(){
+    return (
+    <div>
+      <input></input><button>submit</button>
+      <ul>
+        <li>learning react</li>
+      </ul>
+    </div>
+    )
+  }
+  
+}
+
+export default TodoList;
