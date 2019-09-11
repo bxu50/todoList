fb73b38cd3bfafe2cae90ea3aa6d3cfc2331ff08
Benshuai Xu <bxu50@student.monash.edu>
2019-09-12 00:04:52 +1000

    add and delete

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
   render(){
     return (
     <div>
-      <input></input><button>submit</button>
+      <input value={this.state.inputValue}
+             onChange = {this.handleInputChange.bind(this)}
+              />
+      <button onClick = {this.handleButtonClick.bind(this)}>submit</button>
       <ul>
-        <li>learning react</li>
+        {
+          this.state.list.map((item,index)=>{
+            return <li key={index} onClick= {this.handleDelete.bind(this,index)}>{item}</li>
+          })
+        }
       </ul>
     </div>
     )
+  }
+  handleInputChange(e){
+    console.log(e.target.value);
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleButtonClick(){
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handleDelete(index){
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({
+      list: list
+    })
   }
   
 }
